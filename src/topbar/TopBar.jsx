c6a6db4bf7586b05d936b133e07ac035a386d91d
abdulgits <ided.263@gmail.com>
2024-04-19 19:57:53 +0900

    first commit

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,13 @@
+import  './topbar.css';
+
+const TopBar = () => {
+    return (
+        <div className='top'>
+            <div className='topLeft'></div>
+            <div className='topCenter'></div>
+            <div className='topRight'></div>
+        </div>
+    );
+}
+
+export default TopBar;
